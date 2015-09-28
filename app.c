7d5d83f0a8353f88e1410fcbc46ca84aaac46f33
Vincent Fiestada <vffiestada@upd.edu.ph>
2015-09-28 14:49:36 +0800

    Add read from input file, recognize vertical lines

--- app.c
+++ app.c
@@ -5,9 +5,9 @@
 #include "slist.h"
 #include <time.h> // clock_t, clock(), CLOCKS_PER_SEC
 
-#define H 40
-#define W 40
-#define DEBUG
+#define H 200
+#define W 400
+//#define DEBUG
 
 // Tile states
 #define BLOCKED 1
@@ -22,11 +22,15 @@
 #define STRAT_DFS 2
 #define STRAT_ASTAR 3
 
+#define STRINGMAX 100
+
+// Error codes
+#define ERR_INPUTFILE_CANNOTOPEN 404
+
 // Global variables
 int grid[H][W];
 coordinate pred[H][W]; // Used to keep track of the traversal
 //pred[i][j] = (x,y) means that (i,j) comes after (x,y) in our path
-polygon p;
 
 void setTile(unsigned int x, unsigned int y, unsigned int s);
 unsigned int getTile(unsigned int x, unsigned int y);
@@ -65,72 +69,101 @@ int main()
     }
 
     // Open and parse input file
+    // Get input file's filename
+    char inputFilename[STRINGMAX] = "";
+    printf("\nInput Filename (Max %d characters): ", STRINGMAX - 1);
+    scanf("%s", &inputFilename);
+    printf("\nOpening file '%s'...", inputFilename);
+    printf("\n--------------------------------\n");
 
-    // Some sample vertices
-    // Later, they should be read from an input file
+    FILE * inputFile = fopen(inputFilename, "r");
+    if (inputFile == NULL) // If inputFile is null, then the stream couldn't be opened; report error
+	{
+		fprintf(stderr,"\nFATAL ERROR!\nFailed to open '%s'. ", inputFilename);
+		exit(ERR_INPUTFILE_CANNOTOPEN); // exit with appropriate error code
+	}
+    // Set starting point and goal
+    coordinate current;
+    coordinate goal;
 
-    // A triangle (3 vertices)
-    unsigned int vertices[3][2] =
-    {{20, 4},
-     {12, 12},
-     {24, 23}
-    };
+    fscanf(inputFile, "%d %d %d %d", &(current.x), &(current.y), &(goal.x), &(goal.y));
+    setTile(current.x, current.y, CURRENT);
+    setTile(goal.x, goal.y, GOAL);
 
-    line e[3];
-    for (i = 0; i < 3; i++)
+    printf("\nFind Path from (%d, %d) to (%d, %d).", current.x, current.y, goal.x, goal.y);
+    printf("\nObstacles:\n");
+    unsigned int tempInt;
+    while (fscanf(inputFile, "%d", &tempInt) > 0) // Number of vertices for this polygon
     {
-        coordinate nv; // Get next vertex
-        if (i < 3 - 1)
+        /*  FORMAT OF POLYGONS
+         *  number_of_vertices x1 y1 x2 y2 ...
+         */
+         // Get coordinates of vertices
+        coordinate vertices[tempInt];
+        for (i = 0; i < tempInt; i++)
         {
-            nv.x = vertices[i+1][0];
-            nv.y = vertices[i+1][1];
+            fscanf(inputFile, "%d %d", &(vertices[i].x), &(vertices[i].y));
         }
-        else
+        /* >>>>>>>>> Create Polyon >>>>>>>> */
+        // Determines lines/edges
+        line e[tempInt];
+        for (i = 0; i < tempInt; i++)
         {
-            nv.x = vertices[0][0];
-            nv.y = vertices[0][1];
-        }
-        coordinate c;
-        c.x = vertices[i][0];
-        c.y = vertices[i][1];
-        e[i].m = getSlope(c, nv);
-        e[i].b = getYIntercept(c, e[i].m);
-        e[i].one.x = vertices[i][0];
-        e[i].one.y = vertices[i][1];
-        e[i].two.x = nv.x;
-        e[i].two.y = nv.y;
-    }
-
-    p.edges = e;
-
-    // Set blocked tiles
-    for (i = 0; i < H; i++)
-    {
-        for (j = 0; j < W; j++)
-        {
-            for (k = 0; k < 3; k++)
+            coordinate nv; // Get next vertex
+            if (i < tempInt - 1)
             {
-                coordinate c;
-                c.x = j;
-                c.y = i;
-                line e = p.edges[k];
-                if (inLine(c, &(e)))
+                nv.x = vertices[i+1].x;
+                nv.y = vertices[i+1].y;
+            }
+            else
+            {
+                nv.x = vertices[0].x;
+                nv.y = vertices[0].y;
+            }
+            e[i].m = getSlope(vertices[i], nv);
+            e[i].b = getYIntercept(vertices[i], e[i].m);
+            e[i].x = -1; // The line is NOT vertical
+            // if the line is vertical, store the constant X because slope-intercept won't work
+            if (nv.x == vertices[i].x)
+            {
+                e[i].x = nv.x;
+            }
+            e[i].one.x = vertices[i].x;
+            e[i].one.y = vertices[i].y;
+            e[i].two.x = nv.x;
+            e[i].two.y = nv.y;
+        }
+        // Create new polygon
+        polygon p;
+        p.edges = e;
+        // >>  Set blocked tiles <<
+        // For each point,
+        for (i = 0; i < H; i++)
+        {
+            for (j = 0; j < W; j++)
+            {
+                // For each edge of the polygon
+                for (k = 0; k < tempInt; k++)
                 {
-                    grid[i][j] = BLOCKED;
-                    break;
+                    coordinate c;
+                    c.x = j;
+                    c.y = i;
+                    line e = p.edges[k];
+                    if (inLine(c, &(e)))
+                    {
+                        grid[i][j] = BLOCKED;
+                        break;
+                    }
                 }
             }
         }
+        // Print obstacle vertices
+        for (i = 0; i < tempInt; i++)
+        {
+            printf("(%d %d) ", vertices[i].x, vertices[i].y);
+        }
+        printf("\n");
     }
-    // Set starting point and goal
-    coordinate current;
-    current.x = 0;
-    current.y = 0;
-    setTile(0, 0, CURRENT);
-    coordinate goal;
-    goal.x = 39;
-    goal.y = 5;
-    setTile(goal.x, goal.y, GOAL);
 
     i = 0; // keeps track of number of expanded nodes
     #ifdef DEBUG
@@ -141,8 +174,8 @@ int main()
     printf("\nChoose a Search Strategy\n1 - BFS\n2 - DFS\nOther - A* Search\n>>> Enter Choice: ");
     scanf("%d", &strategy);
 
+    printf("\nStarting Search...\n");
     clock_t t = clock(); // For keeping track of running time
-
     if (strategy == STRAT_BFS)
     {
         // Create fringe queue
@@ -162,7 +195,11 @@ int main()
                 coordinate target = Dequeue(fringe);
                 current = teleport(current, target);
             }
-            else break;
+            else
+            {
+                printf("\n\n <!> No solution path found.");
+                break;
+            }
             i++;
         } while(1);
 
@@ -190,6 +227,11 @@ int main()
             {
                 coordinate target = PopFromStack(fringe);
                 current = teleport(current, target);
+            }
+            else
+            {
+                printf("\n\n <!> No solution path found.");
+                break;
             }
             i++;
         } while(1);
@@ -221,6 +263,11 @@ int main()
                 coordinate target = PopFromSortedList(fringe);
                 current = teleport(current, target);
             }
+            else
+            {
+                printf("\n\n <!> No solution path found.");
+                break;
+            }
             i++;
         } while(1);
 
@@ -248,7 +295,7 @@ int main()
         drawGrid();
     #endif
     printf("\n--------------------------------------------------\n");
-    printf("Current Location is (%d, %d)", current.x, current.y);
+    printf("Final Location is (%d, %d)", current.x, current.y);
     if (getTile(current.x, current.y) == GOAL) printf(" which is a GOAL point.");
     printf("\n\n");
     printf("Traced Path ");
@@ -331,16 +378,10 @@ coordinate teleport(coordinate current, coordinate target)
  */
 void drawGrid()
 {
-    unsigned int i, j, k;
-    printf("    ");
-    for (j = 0; j < W; j++) {
-        printf("%2d ", j);
-    }
+    int i, j;
     printf("\n");
-
     for (i = 0; i < H; i++)
     {
-        printf("%2d ", i);
         for (j = 0; j < W; j++)
         {
             switch(grid[i][j])
--- line.h
+++ line.h
@@ -11,6 +11,8 @@ typedef struct
 {
     float m; // slope of the line
     float b; // y-intercept of the line
+    int x; // One x-coordinate on the line (used for vertical lines for which slope-intercept doesn't work)
+    // IMPORTANT: if x >= 0, the inLine algorithm will assume that the line is vertical with eq. x = some constant
     coordinate one;
     coordinate two;
 } line; // each line is represented by its slope-intercept equation
@@ -20,6 +22,7 @@ typedef struct
  */
 float getSlope(coordinate a, coordinate b)
 {
+    if (b.x - a.x == 0) return 0;
     return ((float)(b.y - a.y))/((float)(b.x - a.x));
 }
 
@@ -37,17 +40,28 @@ float getYIntercept(coordinate a, float m)
  */
 bool inLine(coordinate a, line * l)
 {
-    // (x,y) is in l if it satisfies the equation y = l.m*x + l.b
-    float tY = l->m * (float)a.x + l->b;
-    // also check x = (y-l.b)/(l.m)
-    float tX = ((float)a.y - l->b)/l->m;
-    if (((float)a.y == floor(tY) || (float)a.y == ceil(tY)) || ((float)a.x == floor(tX) || (float)a.x == ceil(tX)))
-    //if (((float)y == floor(tY)) || ((float)x == floor(tX)))
+    if (l->x >= 0) // l.x is nonnegative, that is, l is a vertical line
     {
-        if ((l->one.x <= a.x && a.x <= l->two.x) || (l->two.x <= a.x && a.x <= l->one.x))
+        if (a.x == l->x)
         {
             if ((l->one.y <= a.y && a.y <= l->two.y) || (l->two.y <= a.y && a.y <= l->one.y))
             return true;
+        }
+    }
+    else
+    {
+        // (x,y) is in l if it satisfies the equation y = l.m*x + l.b
+        float tY = l->m * (float)a.x + l->b;
+        // also check x = (y-l.b)/(l.m)
+        float tX = ((float)a.y - l->b)/l->m;
+        if (((float)a.y == floor(tY) || (float)a.y == ceil(tY)) || ((float)a.x == floor(tX) || (float)a.x == ceil(tX)))
+        //if (((float)y == floor(tY)) || ((float)x == floor(tX)))
+        {
+            if ((l->one.x <= a.x && a.x <= l->two.x) || (l->two.x <= a.x && a.x <= l->one.x))
+            {
+                if ((l->one.y <= a.y && a.y <= l->two.y) || (l->two.y <= a.y && a.y <= l->one.y))
+                return true;
+            }
         }
     }
     return false;
